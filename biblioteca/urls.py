937dd1f28db62cf2655e1477cf5dc7e870b34136
Wesley Pereira <aventureiro-msn@hotmail.com>
2023-12-28 18:39:33 -0300

    update lesson 2

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #o include redireciona para urls do app livro
     path('livro/', include('livro.urls')),
+    path('auth/', include('usuarios.urls')),
 ]
--- livro/views.py
+++ livro/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from usuarios.models import Usuario
 from django.http import HttpResponse
 
-def cadastrar(request):
-    return HttpResponse('ola')
-    #return render(request, 'cadastrar.html')
+def home(request):
+    if request.session.get('usuario'):
+        usuario = Usuario.objects.get(id=request.session['usuario']).nome              
+        return HttpResponse(f'ola {usuario}')
+    else:
+        return redirect('/auth/login/?status=2')
+    
